b05a89b1e627a57231480fabf892ef89ecd7c494
Ridhwan Ikhwanto <ridhwan@ikhwanto.com>
2022-02-23 11:25:06 +0700

    Add code for table-driven unit tests

--- src/main_test.go
+++ src/main_test.go
@@ -23,6 +23,29 @@ var wrongPaths = []string{
 
 var clientPath = "/.well-known/matrix/client"
 
+type headerCombination struct {
+	header, value string
+}
+
+type urlCombination struct {
+	url, value string
+}
+
+type serverCombination struct {
+	homeserver, identityserver urlCombination
+}
+
+var serverList = []serverCombination{
+	{homeserver: urlCombination{"CLIENT_HOMESERVER", "rando"},
+		identityserver: urlCombination{"CLIENT_IDENTITYSERVER", "rando2"}},
+	{homeserver: urlCombination{"CLIENT_HOMESERVER", "rando3"},
+		identityserver: urlCombination{"CLIENT_IDENTITYSERVER", "rando4"}},
+	{homeserver: urlCombination{"CLIENT_HOMESERVER", "rando5"},
+		identityserver: urlCombination{"CLIENT_IDENTITYSERVER", "rando6"}},
+	{homeserver: urlCombination{"CLIENT_HOMESERVER", "rando5:444"},
+		identityserver: urlCombination{"CLIENT_IDENTITYSERVER", "rando6:4321"}},
+}
+
 func TestWrongPath(t *testing.T) {
 	for _, test := range wrongPaths {
 		req := httptest.NewRequest(http.MethodGet, test, nil)
@@ -35,39 +58,59 @@ func TestWrongPath(t *testing.T) {
 	}
 }
 
-func TestWithIdentityServer(t *testing.T) {
-	os.Setenv("CLIENT_HOMESERVER", "rando")
-	os.Setenv("CLIENT_IDENTITYSERVER", "rando2")
+func TestMultipleWithIdentityServer(t *testing.T) {
+	for _, testNow := range serverList {
+		var headerList = []headerCombination{
+			{"Access-Control-Allow-Origin", "*"},
+			{"content-type", "application/json;charset=UTF-8"},
+		}
+		var urlList = []urlCombination{
+			testNow.homeserver,
+			testNow.identityserver,
+		}
 
-	req := httptest.NewRequest(http.MethodGet, clientPath, nil)
-	w := httptest.NewRecorder()
-	requestHandler(w, req)
-	result := w.Result()
-	if result.StatusCode != http.StatusOK {
-		t.Errorf("Wrong status code with status code %d that should be 200", result.StatusCode)
-	}
-	if header := result.Header.Get("Access-Control-Allow-Origin"); header != "*" {
-		t.Errorf("Wrong header received at client")
-	}
-	if header := result.Header.Get("content-type"); header != "application/json;charset=UTF-8" {
-		t.Errorf("Wrong header received at client")
-	}
+		for _, env := range urlList {
+			os.Setenv(env.url, env.value)
+		}
 
-	defer result.Body.Close()
-	data, err := ioutil.ReadAll(result.Body)
-	if err != nil {
-		t.Errorf("Expected error to be nil got %v", err)
-	}
+		req := httptest.NewRequest(http.MethodGet, clientPath, nil)
+		w := httptest.NewRecorder()
+		requestHandler(w, req)
+		result := w.Result()
+		if result.StatusCode != http.StatusOK {
+			t.Errorf("Wrong status code with status code %d that should be 200", result.StatusCode)
+		}
 
-	var clientJSON ClientResponse
-	err = json.Unmarshal(data, &clientJSON)
-	if err != nil {
-		t.Errorf("Expected error to be nil got %v", err)
-	}
-	if clientJSON.Homeserver.BaseURL != "rando" {
-		t.Errorf("Wrong homeserver received")
-	}
-	if clientJSON.IdentityServer.BaseURL != "rando2" {
-		t.Errorf("Wrong identity server received")
+		for _, test := range headerList {
+			if headerValue := result.Header.Get(test.header); headerValue != test.value {
+				t.Errorf("Wrong header received at client at header %s with response %s that should be %s",
+					test.header, headerValue, test.value)
+			}
+		}
+
+		defer result.Body.Close()
+		data, err := ioutil.ReadAll(result.Body)
+		if err != nil {
+			t.Errorf("Expected error to be nil got %v", err)
+		}
+
+		var clientJSON ClientResponse
+		err = json.Unmarshal(data, &clientJSON)
+		if err != nil {
+			t.Errorf("Expected error to be nil got %v", err)
+		}
+
+		for _, test := range urlList {
+			if test.url == "CLIENT_HOMESERVER" {
+				if value := clientJSON.Homeserver.BaseURL; value != test.value {
+					t.Errorf("Wrong homeserver received, should be %s but got %s", test.value, value)
+				}
+			} else if test.url == "CLIENT_IDENTITYSERVER" {
+				if value := clientJSON.IdentityServer.BaseURL; value != test.value {
+					t.Errorf("Wrong identity server received, should be %s but got %s", test.value, value)
+				}
+			}
+		}
+
 	}
 }
